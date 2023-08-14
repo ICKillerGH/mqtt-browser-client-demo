1066eb4529a75f87696de42eeb83caae9536da0e
Alexis Navarro <alexisnavarro1994@gmail.com>
2023-08-14 10:19:50 -0400

    Add missing buttons and adjust layout

--- js/index.js
+++ js/index.js
@@ -8,11 +8,77 @@ const POWER_STATE = Object.freeze({
   ON: true,
 });
 
+const MODES = [
+  {
+    name: "Temperatura",
+    icon: `
+      <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="w-16 h-16"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M10 13.5a4 4 0 1 0 4 0v-8.5a2 2 0 0 0 -4 0v8.5"></path>
+          <path d="M10 9l4 0"></path>
+      </svg>
+    `,
+  },
+  {
+    name: "Smart",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-16 h-16"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+          d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7"
+        ></path>
+        <path
+          d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3"
+        ></path>
+        <path d="M9.7 17l4.6 0"></path>
+      </svg>
+    `,
+  },
+  {
+    name: "Eco",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-16 h-16+"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M18.364 19.364a9 9 0 1 0 -12.728 0"></path>
+        <path d="M15.536 16.536a5 5 0 1 0 -7.072 0"></path>
+        <path d="M12 13m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
+      </svg>
+    `,
+  },
+];
+
 const app = () => ({
   selectedUser: null,
   esp32s: [],
   sysState: POWER_STATE.OFF,
-  mode: 1,
+  mode: MODES[0].name,
   targetTemperature: 0,
   temperature: 0,
   targetPower: 0,
@@ -147,6 +213,9 @@ const app = () => ({
   get sysStateIsOff() {
     return this.sysState === POWER_STATE.OFF;
   },
+  get modes() {
+    return MODES;
+  },
   sendMqttMessage(topic) {
     return (value) => {
       const message = JSON.stringify({
@@ -190,6 +259,12 @@ const app = () => ({
     900,
     { maxWait: "5000" }
   ),
+  isModeSelected(mode) {
+    return mode === this.mode;
+  },
+  setMode(mode) {
+    this.mode = mode;
+  },
 });
 
 const rangeSlider = ({
