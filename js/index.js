9f9455b3e83248e638fd51eb9a8678dfbcfb72f4
Alexis Navarro <alexisnavarro1994@gmail.com>
2023-08-23 10:17:28 -0400

    Add device status

--- js/index.js
+++ js/index.js
@@ -98,11 +98,187 @@ const MODES = [
   },
 ];
 
+const STATUS = {
+  65537: {
+    description: "Operativo",
+    class: "text-green-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+          d="M11.998 2l.118 .007l.059 .008l.061 .013l.111 .034a.993 .993 0 0 1 .217 .112l.104 .082l.255 .218a11 11 0 0 0 7.189 2.537l.342 -.01a1 1 0 0 1 1.005 .717a13 13 0 0 1 -9.208 16.25a1 1 0 0 1 -.502 0a13 13 0 0 1 -9.209 -16.25a1 1 0 0 1 1.005 -.717a11 11 0 0 0 7.531 -2.527l.263 -.225l.096 -.075a.993 .993 0 0 1 .217 -.112l.112 -.034a.97 .97 0 0 1 .119 -.021l.115 -.007zm3.71 7.293a1 1 0 0 0 -1.415 0l-3.293 3.292l-1.293 -1.292l-.094 -.083a1 1 0 0 0 -1.32 1.497l2 2l.094 .083a1 1 0 0 0 1.32 -.083l4 -4l.083 -.094a1 1 0 0 0 -.083 -1.32z"
+          stroke-width="0"
+          fill="currentColor"
+        ></path>
+      </svg>
+  `,
+  },
+  65538: {
+    description: "Sobretemperatura superior",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M8 13.5a4 4 0 1 0 4 0v-8.5a2 2 0 0 0 -4 0v8.5"></path>
+        <path d="M8 9l4 0"></path>
+        <path d="M16 9l6 0"></path>
+        <path d="M19 6l0 6"></path>
+      </svg>
+  `,
+  },
+  65539: {
+    description: "Sobretemperatura inferior",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M8 13.5a4 4 0 1 0 4 0v-8.5a2 2 0 0 0 -4 0v8.5"></path>
+        <path d="M8 9l4 0"></path>
+        <path d="M16 9l6 0"></path>
+      </svg>
+  `,
+  },
+  65540: {
+    description: "Sobretemperatura general",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M10 13.5a4 4 0 1 0 4 0v-8.5a2 2 0 0 0 -4 0v8.5"></path>
+        <path d="M10 9l4 0"></path>
+      </svg>
+  `,
+  },
+  65541: {
+    description: "En recuperacion térmica",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4"></path>
+        <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"></path>
+      </svg>
+  `,
+  },
+  65542: {
+    description: "Error de sensor superior",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+          d="M9 12h-3.586a1 1 0 0 1 -.707 -1.707l6.586 -6.586a1 1 0 0 1 1.414 0l6.586 6.586a1 1 0 0 1 -.707 1.707h-3.586v6h-6v-6z"
+        ></path>
+        <path d="M9 21h6"></path>
+      </svg>
+  `,
+  },
+  65543: {
+    description: "Error de sensor inferior",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path
+          d="M15 12h3.586a1 1 0 0 1 .707 1.707l-6.586 6.586a1 1 0 0 1 -1.414 0l-6.586 -6.586a1 1 0 0 1 .707 -1.707h3.586v-6h6v6z"
+        ></path>
+        <path d="M15 3h-6"></path>
+      </svg>
+  `,
+  },
+  65544: {
+    description: "Error de ambos sensores",
+    class: "text-red-500",
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-20 h-20"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M7 3l0 18"></path>
+        <path d="M10 6l-3 -3l-3 3"></path>
+        <path d="M20 18l-3 3l-3 -3"></path>
+        <path d="M17 21l0 -18"></path>
+      </svg>
+  `,
+  },
+};
+
 const app = () => ({
   selectedUser: null,
   esp32s: [],
   sysState: POWER_STATE.OFF,
   mode: MODES[0].name,
+  status: 65537,
   temperature: 0,
   targetTemperature: 0,
   targetSmart: 0,
@@ -148,6 +324,9 @@ const app = () => ({
           break;
         case this.modeTopic(this.selectedUser):
           this.mode = value;
+          break;
+        case this.statusTopic(this.selectedUser):
+          this.status = value;
           break;
         case this.temperatureSensorTopic(this.selectedUser):
           this.temperature = value;
@@ -195,6 +374,7 @@ const app = () => ({
   resetValues() {
     this.sysState = POWER_STATE.OFF;
     this.mode = MODES[0].name;
+    this.status = 65537;
     this.temperature = 0;
     this.targetTemperature = 0;
     this.targetSmart = 0;
@@ -208,6 +388,7 @@ const app = () => ({
     return [
       this.sysStateTopic(id),
       this.modeTopic(id),
+      this.statusTopic(id),
       this.temperatureSensorTopic(id),
       this.targetTemperatureTopic(id),
       this.targetEcoTopic(id),
@@ -221,6 +402,9 @@ const app = () => ({
   },
   modeTopic(id) {
     return `${id}/mode`;
+  },
+  statusTopic(id) {
+    return `${id}/status`;
   },
   temperatureSensorTopic(id) {
     return `${id}/temperature`;
@@ -248,6 +432,9 @@ const app = () => ({
   },
   get modes() {
     return MODES;
+  },
+  get statuses() {
+    return STATUS;
   },
   sendMqttMessage(topic) {
     return (value) => {
